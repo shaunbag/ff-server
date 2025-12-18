f1f8f560abb258dd2a564f0dcb5c74840d79c1f4
Shaun <shaun@vegascity,org>
2025-12-18 21:09:47 +0000

    progress model

---
+++ src/main/java/com/shaunbag/ff_server/model/progress.java
@@ -0,0 +1,25 @@
+package com.shaunbag.ff_server.model;
+
+public class progress {
+
+    private String book;
+
+    private Integer section;
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public Integer getSection() {
+        return section;
+    }
+
+    public void setSection(Integer section) {
+        this.section = section;
+    }
+
+}
